e248f68df0809d0433e6e67ca126239015ef62ae
abde chkir <abdechkir@example.com>
2024-03-27 05:57:18 +0000

    Updated

--- GreatDeals/settings.py
+++ GreatDeals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'lbal-shop-614bd7cbb3bf.herokuapp.com']
 
 
 # Application definition
