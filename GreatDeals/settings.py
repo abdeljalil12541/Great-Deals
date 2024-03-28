b9baa3944cce4e86805d73a8c44229833d47c20a
abde chkir <abdechkir@example.com>
2024-03-28 07:00:09 +0000

    updated

--- GreatDeals/settings.py
+++ GreatDeals/settings.py
@@ -137,11 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'GreatDeals' / 'static',
+# ]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'GreatDeals' / 'static',
+    os.path.join(BASE_DIR, 'GreatDeals/static')  # 'lbalshop/static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Media files configuration
