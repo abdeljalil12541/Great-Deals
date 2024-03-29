c6acc1e0dea1a6b2216b214a748f5827916e28ce
abde chkir <abdechkir@example.com>
2024-03-29 17:55:32 +0000

    updated

--- GreatDeals/settings.py
+++ GreatDeals/settings.py
@@ -158,7 +158,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # MEDIA_ROOT = BASE_DIR, 'media'
 # MEDIA_URL = '/media/'
